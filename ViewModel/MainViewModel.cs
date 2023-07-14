dfdc48687e36a8d1b6dca4a85c9f74a434802b4c
ATravelingWizard <henardgertzen@gmail.com>
2023-07-14 19:25:47 +0200

    Gave nav-menu functionality

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using FontAwesome.Sharp;
 using Skreenkinikor_Project.Model;
 using Skreenkinikor_Project.Repositories;
 
@@ -14,6 +16,9 @@ namespace Skreenkinikor_Project.ViewModel
     {
         //Fields
         private AccountModel _currentUser;
+        private VMBase _currentVMChild;
+        private string _caption;
+        private IconChar _icon;
         private IUserRepo _userRepo;
 
         public AccountModel CurrentUser 
@@ -29,11 +34,104 @@ namespace Skreenkinikor_Project.ViewModel
             }
         }
 
+        //Properties
+        public VMBase CurrentVMChild
+        {
+            get
+            {
+                return _currentVMChild;
+            }
+            set
+            {
+                _currentVMChild = value;
+                OnPropertyChanged(nameof(CurrentVMChild));
+            }
+        }
+        public string Caption
+        {
+            get => _caption;
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+        public IconChar Icon
+        {
+            get => _icon;
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        //Commands
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowActorViewCommand { get; }
+        public ICommand ShowMovieViewCommand { get; }
+        public ICommand ShowConfectionaryViewCommand { get; }
+        public ICommand ShowReportsViewCommand { get; }
+        public ICommand ShowSettingsViewCommand { get; }
+
         public MainViewModel()
         {
             _userRepo = new UserRepo();
             CurrentUser = new AccountModel();
+
+            //Initialize Commands
+            ShowHomeViewCommand = new VMCommand(ExecuteHomeViewCommand);
+            ShowActorViewCommand = new VMCommand(ExecuteShowActorViewCommand);
+            ShowMovieViewCommand = new VMCommand(ExecuteShowMovieViewCommand);
+            ShowConfectionaryViewCommand = new VMCommand(ExecuteConfectionaryViewCommand);
+            ShowReportsViewCommand = new VMCommand(ExecuteReportsViewCommand);
+            ShowSettingsViewCommand = new VMCommand(ExcuteSettingsViewCommand);
+
+
+            ExecuteHomeViewCommand(null);
             LoadCurrent();
+        }
+
+        private void ExcuteSettingsViewCommand(object obj)
+        {
+            CurrentVMChild = new VMSettings();
+            Caption = "Settings";
+            Icon = IconChar.Gears;
+        }
+
+        private void ExecuteReportsViewCommand(object obj)
+        {
+            CurrentVMChild = new VMReports();
+            Caption = "Reports";
+            Icon = IconChar.File;
+        }
+
+        private void ExecuteConfectionaryViewCommand(object obj)
+        {
+            CurrentVMChild = new VMConfectionary();
+            Caption = "Confectionary";
+            Icon = IconChar.BowlFood;
+        }
+
+        private void ExecuteShowMovieViewCommand(object obj)
+        {
+            CurrentVMChild = new VMMovies();
+            Caption = "Movies";
+            Icon = IconChar.Video;
+        }
+
+        private void ExecuteShowActorViewCommand(object obj)
+        {
+            CurrentVMChild = new VMActors();
+            Caption = "Actors";
+            Icon = IconChar.Person;
+        }
+
+        private void ExecuteHomeViewCommand(object obj)
+        {
+            CurrentVMChild = new VMHome();
+            Caption = "Home";
+            Icon = IconChar.Home;
         }
 
         private void LoadCurrent()
@@ -42,7 +140,7 @@ namespace Skreenkinikor_Project.ViewModel
             if(user != null)
             {
                 CurrentUser.Username = user.Username;
-                CurrentUser.DisplayUser = $"Welcome {user.Name} {user.Lastname}";
+                CurrentUser.DisplayUser = $"{user.Name} {user.Lastname}";
                 CurrentUser.pfp = null;
             }
             else
