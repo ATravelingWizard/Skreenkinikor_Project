3c223c53075706b86d2023e193146cc43f7725ef
ATravelingWizard <henardgertzen@gmail.com>
2023-07-14 16:07:39 +0200

    Finished mainform ui layout and allowed for window resizing on dynamic monitor changes

--- View/MainView.xaml.cs
+++ View/MainView.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace Skreenkinikor_Project.View
 {
@@ -22,6 +24,20 @@ namespace Skreenkinikor_Project.View
         public MainView()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void pnlControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161,2,0);
+        }
+
+        private void pnlControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
     }
 }
